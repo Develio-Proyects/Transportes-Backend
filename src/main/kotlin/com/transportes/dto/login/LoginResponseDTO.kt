fd9c7f8054aaaae6d48fbdbff626c7d3b3cf20c9
tobiasriccone <tobi_riccone@hotmail.com>
2025-08-03 11:44:40 -0300

    Agrega info al response del login

--- src/main/kotlin/com/transportes/dto/login/LoginResponseDTO.kt
+++ src/main/kotlin/com/transportes/dto/login/LoginResponseDTO.kt
@@ -1,7 +1,9 @@
 package com.transportes.dto.login
 
 data class LoginResponseDTO(
-    val role: String,
+    val userId: String,
     val name: String,
+    val email: String,
+    val role: String,
     val token: String
 )
